68e91a12def4fd66188d1983066ebf93813e8b03
kingkong345 <babyiamokay@gmail.com>
2023-01-09 22:03:19 +0900

    category js 추가

--- src/js/category.js
+++ src/js/category.js
@@ -3,33 +3,40 @@ $(function () {
     if (type === "signature") {
       $(".div1").show();
       $(".category-center-menu p.signature").show();
+      $(".left-bg .bg-item.signature-bg").show();
     } else {
       $(".div1").hide();
       $(".category-center-menu p.signature").hide();
+      $(".left-bg .bg-item.signature-bg").hide();
     }
-
     if (type === "beverage") {
       $(".div2").show();
       $(".category-center-menu p.beverage").show();
+      $(".left-bg .bg-item.beverage-bg").show();
     } else {
       $(".div2").hide();
       $(".category-center-menu p.beverage").hide();
+      $(".left-bg .bg-item.beverage-bg").hide();
     }
 
     if (type === "coffee") {
       $(".div3").show();
       $(".category-center-menu p.coffee").show();
+      $(".left-bg .bg-item.coffee-bg").show();
     } else {
       $(".div3").hide();
       $(".category-center-menu p.coffee").hide();
+      $(".left-bg .bg-item.coffee-bg").hide();
     }
 
     if (type === "cookie") {
       $(".div4").show();
       $(".category-center-menu p.cookie").show();
+      $(".left-bg .bg-item.cookie-bg").show();
     } else {
       $(".div4").hide();
       $(".category-center-menu p.cookie").hide();
+      $(".left-bg .bg-item.cookie-bg").hide();
     }
   }
 
@@ -49,4 +56,13 @@ $(function () {
   $("#category button.cookie").on("click", function () {
     showItem("cookie");
   });
+});
+
+$("#category .category-btn .category-btn-list button").click(function () {
+  if ($(this).hasClass("active")) {
+    $("#category .category-btn .category-btn-list button").removeClass("active");
+  } else {
+    $("#category .category-btn .category-btn-list button").removeClass("active");
+    $(this).addClass("active");
+  }
 });
