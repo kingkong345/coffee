646e9c432c0d0d78b5c99c488c0e58e31dddfaab
kingkong345 <babyiamokay@gmail.com>
2023-01-10 22:32:43 +0900

    category is-active js 추가

--- build/js/category.js
+++ build/js/category.js
@@ -1,42 +1,50 @@
 $(function () {
   function showItem(type) {
+    $("#category .category-btn .category-btn-list button").removeClass("active");
+    $("#category .left-bg .bg-item").removeClass("is-active");
+    $(".left-bg .bg-item").css("z-index", 0);
+
     if (type === "signature") {
       $(".div1").show();
       $(".category-center-menu p.signature").show();
-      $(".left-bg .bg-item.signature-bg").show();
+      $(".left-bg .bg-item.cookie-bg").css("z-index", 1);
+      $(".left-bg .bg-item.signature-bg").addClass("is-active").css("z-index", 2);
+      $(".category-btn button.signature").addClass("active");
     } else {
       $(".div1").hide();
       $(".category-center-menu p.signature").hide();
-      $(".left-bg .bg-item.signature-bg").hide();
     }
     if (type === "beverage") {
       $(".div2").show();
       $(".category-center-menu p.beverage").show();
-      $(".left-bg .bg-item.beverage-bg").show();
+      $(".left-bg .bg-item.signature-bg").css("z-index", 1);
+      $(".left-bg .bg-item.beverage-bg").addClass("is-active").css("z-index", 2);
+      $(".category-btn button.beverage").addClass("active");
     } else {
       $(".div2").hide();
       $(".category-center-menu p.beverage").hide();
-      $(".left-bg .bg-item.beverage-bg").hide();
     }
 
     if (type === "coffee") {
       $(".div3").show();
       $(".category-center-menu p.coffee").show();
-      $(".left-bg .bg-item.coffee-bg").show();
+      $(".left-bg .bg-item.beverage-bg").css("z-index", 1);
+      $(".left-bg .bg-item.coffee-bg").addClass("is-active").css("z-index", 2);
+      $(".category-btn button.coffee").addClass("active");
     } else {
       $(".div3").hide();
       $(".category-center-menu p.coffee").hide();
-      $(".left-bg .bg-item.coffee-bg").hide();
     }
 
     if (type === "cookie") {
       $(".div4").show();
       $(".category-center-menu p.cookie").show();
-      $(".left-bg .bg-item.cookie-bg").show();
+      $(".left-bg .bg-item.coffee-bg").css("z-index", 1);
+      $(".left-bg .bg-item.cookie-bg").addClass("is-active").css("z-index", 2);
+      $(".category-btn button.cookie").addClass("active");
     } else {
       $(".div4").hide();
       $(".category-center-menu p.cookie").hide();
-      $(".left-bg .bg-item.cookie-bg").hide();
     }
   }
 
@@ -56,13 +64,4 @@ $(function () {
   $("#category button.cookie").on("click", function () {
     showItem("cookie");
   });
-});
-
-$("#category .category-btn .category-btn-list button").click(function () {
-  if ($(this).hasClass("active")) {
-    $("#category .category-btn .category-btn-list button").removeClass("active");
-  } else {
-    $("#category .category-btn .category-btn-list button").removeClass("active");
-    $(this).addClass("active");
-  }
 });
