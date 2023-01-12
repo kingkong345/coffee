602c20dea42664b0b9ac71983c15fb33087def9d
kingkong345 <babyiamokay@gmail.com>
2023-01-12 18:26:39 +0900

    header 추가

--- build/js/main.js
+++ build/js/main.js
@@ -10,99 +10,52 @@ const categoryBox = new Swiper("#category .category-box .swiper", {
   spaceBetween: 50,
 });
 
-// const pjSwiper = new Swiper("#projects .mask .banner", {
-//   slidesPerView: 1,
-//   spaceBetween: 50,
-//   centeredSlides: true,
-//   // autoplay: {
-//   //   delay: 5000,
-//   //   disableOnInteraction: false,
-//   // },
-//   loop: true,
-//   loopAdditionalSlides: 1,
-//   breakpoints: {
-//     640: {
-//       slidesPerView: 1,
-//     },
-//     1280: {
-//       slidesPerView: 1,
-//     },
-//   },
-//   pagination: {
-//     el: ".projects-pagination",
-//     clickable: true,
-//   },
-//   navigation: {
-//     nextEl: "#projects .btn-next",
-//     prevEl: "#projects .btn-prev",
-//   },
-// });
+$(".count-num").each(function () {
+  const $this = $(this),
+    countTo = $this.attr("data-count");
 
-// const skill = {
-//   ui: 90,
-//   illust: 95,
-//   photoshop: 88,
-//   figma: 89,
-//   HTML5: 85,
-//   css3: 80,
-//   js: 70,
-// };
-
-// const myMarker = {
-//   fontSize: 20,
-//   indent: 20,
-//   startColor: "#000",
-//   endColor: "#00f",
-// };
-// gsap.defaults({ ease: "power4", duration: 1 });
-
-// const skillTL = gsap.timeline({
-//   scrollTrigger: {
-//     trigger: "#skills",
-//     pin: true,
-//     //pinSpacing: false,
-//     scrub: 1.5,
-//     // markers: myMarker,
-//     start: "top top",
-//     end: "bottom+=200% top",
-//   },
-// });
-// skillTL.to("path", { strokeDashoffset: 0, duration: 5 }).from(skill, {
-//   ui: 0,
-//   illust: 0,
-//   photoshop: 0,
-//   figma: 0,
-//   HTML5: 0,
-//   css3: 0,
-//   js: 0,
-//   onUpdate: function () {
-//     $("#skills .skill-txt:nth-child(2) .num .txt").text(Math.round(skill.ui));
-//     $("#skills .skill-txt:nth-child(3) .num .txt").text(Math.round(skill.illust));
-//     $("#skills .skill-txt:nth-child(4) .num .txt").text(Math.round(skill.photoshop));
-//     $("#skills .skill-txt:nth-child(5) .num .txt").text(Math.round(skill.figma));
-//     $("#skills .skill-txt:nth-child(6) .num .txt").text(Math.round(skill.HTML5));
-//     $("#skills .skill-txt:nth-child(7) .num .txt").text(Math.round(skill.css3));
-//     $("#skills .skill-txt:nth-child(8) .num .txt").text(Math.round(skill.js));
-//   },
-// });
+  $({
+    countNum: $this.text(),
+  }).animate(
+    {
+      countNum: countTo,
+    },
+    {
+      duration: 3000,
+      easing: "linear",
+      step: function () {
+        $this.text(Math.floor(this.countNum));
+      },
+      complete: function () {
+        $this.text(this.countNum.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+      },
+    }
+  );
+});
 
 const header = $("#header");
 const gnb = $("#gnb");
 const btnTop = $(".btn-top");
 
-// btnTop.on("click", function () {
-//   gsap.to(window, { scrollTo: 0, duration: 1 });
-// });
+$(gnb).mouseenter(function () {
+  $(this).addClass("on");
+});
+$(gnb).mouseleave(function () {
+  $(this).removeClass("on");
+});
 
-// header.click(function () {
-//   $(this).siblings().toggleClass("on");
-// });
+$(function () {
+  var lastScrollTop = 0,
+    delta = 15;
+  $(window).scroll(function (event) {
+    var st = $(this).scrollTop();
 
-// const categoryBtn = $("#category .category-btn");
-
-// categoryBtn.on("click", function () {
-//   const leftBg = $("#category .left-bg");
-//   const rightBg = $("#category category-title");
-//   leftBg.show();
-//   console.log(leftBg);
-// });
+    if (Math.abs(lastScrollTop - st) <= delta) return;
+    if (st > lastScrollTop && lastScrollTop > 0) {
+      $("#header").css("top", "-100px");
+    } else {
+      $("#header").css("top", "0px");
+    }
+    lastScrollTop = st;
+  });
+});
