61f0adef3c31a9bacfa17a876b75571d4bc8ae76
kingkong345 <babyiamokay@gmail.com>
2023-01-27 15:06:18 +0900

    btn js 변경

--- src/js/main.js
+++ src/js/main.js
@@ -148,5 +148,5 @@ $(document).ready(function ($) {
  * top 버튼 조작 이벤트
  */
 btnTop.on("click", function () {
-  gsap.to(window, { scrollTo: 0, duration: 1 });
+  gsap.to(window, { duration: 2, scrollTo: 0 });
 });
